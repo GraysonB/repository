2ff59e2faf14d5db8217cd8bcd68dde33900976b
evillatoro1 <villatoroedwin1@gmail.com>
2016-09-05 22:38:42 -0400

    Implemented method calc() in Person5.java

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,7 +30,6 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
 	  return input.substring(2) + input.substring(0,2);
 	}
 	
