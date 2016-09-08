a33adcdf3f107a172da25ef360cedb3487d4418d
jordanparrish <jordanparrish53@gmail.com>
2016-09-08 15:47:33 -0400

    Implemented method calc()

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string
  *
- *  @author Jordan Parrish
+ *  @author Jordan
  *  @version 1.1
  */
 public class Person4 {
