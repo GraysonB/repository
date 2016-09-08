eeee1ed20ad9f41c71fcfc4c747e033ca67a5b89
Abhinav <abhinav.s97@gmail.com>
2016-09-08 17:13:09 -0400

    Updated author name and comments.

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
+ *  @author  Abhinav Sundaresan
  *  @version 1.1
  */
 public class Person3 {
@@ -42,7 +42,6 @@ public class Person3 {
 	 * @return the reversed string
 	 */
     private String calc(String input) {
-        //Person 3 put your implementation here
         char[] character = input.toCharArray();
         int start = 0;
         int end = character.length - 1;
