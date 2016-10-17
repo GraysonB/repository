69f9c3ab949d7be9d59629aebce6966564c996cc
evillatoro1 <villatoroedwin1@gmail.com>
2016-10-17 11:14:57 -0400

    main in application controller cleaned up

--- M4_CleanIO/src/controller/Main_InApplication_Controller.java
+++ M4_CleanIO/src/controller/Main_InApplication_Controller.java
@@ -4,17 +4,16 @@ import fxapp.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import model.Profile;
-import model.WaterSourceReport;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+/**
+ * controller for the main in application screen
+ */
 public class Main_InApplication_Controller {
 
     /** a link back to the main application class */
     private Main mainApplication;
 
-    /** the profile whose data is being manipulated*/
+    /** the profile who is logged in*/
     private Profile profile;
 
     @FXML
@@ -38,20 +37,6 @@ public class Main_InApplication_Controller {
         welcomeUser.setText("Welcome " + profile.getUsername());
     }
 
-
-//    /**
-//     * called when the user clicks submit water source report
-//     */
-//    @FXML
-//    private void handleSubmitWaterSourceReportPressed() {
-//        //mainApplication.getWaterSourceReportController().setProfile(profile);
-//
-////        mainApplication.getWaterSourceReportController().setWaterSourceReport(new WaterSourceReport());
-////        mainApplication.displayWaterSourceReportScene();
-//        mainApplication.getWaterSourceReportController().setProfile(profile);
-//        mainApplication.displayWaterSourceReportOverviewScene();
-//    }
-
     /**
      * called when the user clicks edit profile
      */
@@ -70,30 +55,19 @@ public class Main_InApplication_Controller {
     }
 
     /**
-     * called when user clicks View Water Availability
-     * should display google map when pressed
+     * called when user clicks view water availability
      */
     @FXML
     private void handleViewWaterAvailabilityPressed() {
         mainApplication.displayWaterAvailabilityScene();
     }
 
-    @FXML
-    private void handleSubmitWaterReportPressed() {
-        Date yeah = new Date();
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("MM/dd/yyyy");
-        SimpleDateFormat timeFormatter = new SimpleDateFormat("h:mm a");
-        String date = dateFormatter.format(yeah);
-        String time = timeFormatter.format(yeah);
-        mainApplication.getWaterSourceReportController().setProfile(profile);
-        mainApplication.getWaterSourceReportController().setWaterSourceReport(new WaterSourceReport(date, time));
-        //showWaterSourceReportDetails();
-        //mainApplication.getWaterSourceReportController().setProfile();
-        mainApplication.displayWaterSourceReportScene();
-    }
-
+    /**
+     * called when user clicks view water source reports
+     */
     @FXML
     private void handleViewWaterSourceReportsPressed(){
         mainApplication.displayWaterSourceReportOverviewScene();
     }
+
 }
